944ef8e5cce0d6e02f73a82f07ae8fab9b328a81
adhirajChauhan <adhirajsingh0105@gmail.com>
2023-06-08 08:03:47 +0530

    adding getline code

--- B.tech PEP/CPP Fundamentals/strings.cpp
+++ B.tech PEP/CPP Fundamentals/strings.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
 int main()
 {
     // string myString = "abc";
 
-    string a = "100";
-    string b = "200";
+    // string a = "100";
+    // string b = "200";
 
-    cout << a + b << endl;
+    // cout << a + b << endl;
 
-    string dummyString = "fdkvbefvbfdjkvbdfhkvhkd";
-    cout << dummyString.length();
+    // string dummyString = "Hello World";
+    // cout << dummyString.length();
+    // dummyString[1] = 'l';
+    // cout << "Index : " << dummyString[1] << endl;
+    // cout << dummyString;
 
-    //Adding a dummy line
-    //Adding a dummy line2
+    string fullName;
+    cout << "Type your full name ";
+    // cin >> fullName;
+    getline(cin,fullName);
+    cout << "Full name is : " << fullName;
 
 }
 
