cad4e6fb3cb1b094d9e3f38b26a09acff3dfe628
adhirajChauhan <adhirajsingh0105@gmail.com>
2023-06-08 07:29:35 +0530

    Initial commit

---
+++ B.tech PEP/CPP Fundamentals/strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // string myString = "abc";
+
+    string a = "100";
+    string b = "200";
+
+    cout << a + b << endl;
+
+    string dummyString = "fdkvbefvbfdjkvbdfhkvhkd";
+    cout << dummyString.length();
+}
+
---
+++ B.tech PEP/CPP Fundamentals/variables.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // type variableName = value;
+    // const int my_int = 5;
+    // my_int = 10;
+    // double y = 10.5;
+    // float z = 11.5f;
+
+    // cout << my_int << endl;
+    // cout << z;
+
+    string userName;
+    cout << "Enter your name" << endl;
+    cin >> userName;
+    cout << "Your name is : " << userName;
+
+}
+
+
+
+
+
+//Write a program to take user input for two numbers of type int from the user
+//add those two numbers and store the sum in a third variable of type int
+//and display the result of third variable
+
+//Write a program to take user input for first name and last name of the user
+//add FN and LN in a third variable of type string
+//and display the result of third variable
+
+
+// + - * / % arithmatic
+// == >= <= != > <
+// && ||
